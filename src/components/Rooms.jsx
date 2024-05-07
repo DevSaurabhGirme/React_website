02c420f05f9d94b2b127efc151a322fc010f78eb
Saurabh <sygirme13@gmail.com>
2024-05-07 15:34:02 +0530

    Rooms description update

--- src/components/Rooms.jsx
+++ src/components/Rooms.jsx
@@ -4,8 +4,8 @@ const Rooms = () => {
     return (
         <div className="max-w-[1400px] h-[500px] bg-blue-100 mx-auto my-20 pt-16 lg:mb-[20%] md:mb-[35%] px-4 grid lg:grid-cols-3 gap-4">
             <div className="lg:top-20 relative lg:col-span-1 col-span-2">
-                <h3 className="text-2xl font-bold">Fine Interier Rooms</h3>
-                <p className="pt-4">loren ipsumszdfmse dnfdf dsfawedfknsadlgasdv</p>
+                <h3 className="text-3xl font-bold">Fine Interier Rooms</h3>
+                <p className="pt-4">Indulge in the epitome of luxury and comfort with our exquisite selection of fine rooms and interiors. From opulent suites overlooking panoramic cityscapes to cozy countryside retreats adorned with elegant decor, each space is meticulously designed to elevate your stay to new heights of sophistication. Immerse yourself in a world of refinement and tranquility, where every detail is curated to perfection, promising a truly unforgettable escape.</p>
             </div>
             
             <div className="grid grid-cols-2 col-span-2">
