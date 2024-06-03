70571b715b107d92ec97f2082ea8636f8e663e5c
Saurabh <sygirme13@gmail.com>
2024-06-03 23:21:36 +0530

    parallax bg added alonmg with tagline update and minor font related updates

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,25 +1,21 @@
-import React from 'react'
+import React from 'react';
 
 const Hero = () => {
     return (
-        <div className='w-full h-screen'>
-            <img className='top-0 left-0 w-full h-screen object-cover' src="https://images.pexels.com/photos/38238/maldives-ile-beach-sun-38238.jpeg" alt="/" />
-            <div className='bg-black/30 absolute top-0 left-0 w-full h-screen'/>
-            <div className='absolute top-0 w-full h-full flex flex-col justify-center text-white'>
-                <div className = 'md:left-[10%] max-width-w-[1100px] m-auto absolute p-4'>
-                    <p>All Inclusive</p>
-                    <h1 className='font-bold text-5xl md:text-7xl drop-shadow-2x'>Private Beaches and Getaways</h1>
-                    <p className='max-w-[600px] drop-shadow-2xl py-2 text-xl'>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas sagittis egestas ipsum nec laoreet. 
-                        Donec magna turpis, pellentesque sed varius non, finibus eget mi. 
-                        In varius enim dictum, vehicula lorem ut, viverra odio.
+        <div className='w-full h-screen parallax-bg'>
+            <div className='bg-black/50 absolute top-0 left-0 w-full h-screen'/>
+            <div className='absolute top-0 w-full h-full flex flex-col justify-center text-white font-montserrat'>
+                <div className='md:left-[10%] max-width-w-[1100px] m-auto absolute p-4'>
+                    <p className='text-gray-300 text-lg'>All Inclusive</p>
+                    <h1 className='font-bold text-5xl md:text-7xl drop-shadow-2xl text-white'>Private Beaches and Getaways</h1>
+                    <p className='max-w-[600px] drop-shadow-2xl py-2 text-xl text-gray-300'>
+                        Escape to paradise with our exclusive all-inclusive packages. Enjoy pristine private beaches, luxurious accommodations, and world-class amenities. Whether you're seeking adventure or relaxation, we have everything you need for an unforgettable vacation. Book your dream getaway now and experience the ultimate in luxury and comfort.
                     </p>
-                    <button className='bg-white text-black'>Reserve Now</button>
+                    <button className='bg-white text-black font-bold py-2 px-4 mt-4'>Reserve Now</button>
                 </div>
             </div>
-        
         </div>
-    )
+    );
 }
 
-export default Hero
+export default Hero;
